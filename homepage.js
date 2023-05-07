06d27f5687f55c24234fe9724ae61ebf05d2ebd9
hlx13026 <126851104+hlx13026@users.noreply.github.com>
2023-05-07 12:43:33 +0800

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,84 @@
+function begin(){
+  var response = prompt("What is your name?");
+  alert("Hello"+response+",let's start raising your chick!");
+}
+
+
+
+function aq11(){
+  alert("叽咕");
+}
+function aq12(){
+  alert("哇，这难道就是传说中的小天才饲料吗，开心(๑>؂<๑）");
+
+}function aq13(){
+  alert("我只是一只小鸡，不吃棒棒糖(눈_눈)");
+}function aq14(){
+  alert("(ﾉಥ  ಥ)");
+}
+function aq21(){
+  alert("(¦3[▓▓]");
+}
+function aq22(){
+  alert("(｡•ˇ‸ˇ•｡)好吧");
+}function aq23(){
+  alert("Ծ‸Ծ叽咕");
+}function aq24(){
+  alert("(｡･ω･｡)ﾉ♡叽哇，有小天才手表连觉都不想睡了唉");
+}
+
+
+function bq11(){
+  alert("真的吗");}
+
+  function bq12(){
+    alert("嘿咻嘿咻");}
+    function bq13(){
+      alert("。。zzz");}
+      function bq14(){
+        alert("对了！就是这样帅气的发型ψ(｀∇´)ψ");}
+
+        function bq21(){
+        alert("( ✘_✘ )↯");}
+        function bq22(){
+        alert("( ✘_✘ )↯");}
+        function bq23(){
+        alert("（｡ò ∀ ó｡）");}
+        function bq24(){
+        alert("( ✘_✘ )↯");}
+
+function cq11(){
+alert("不要打扰我");}
+function cq12(){
+alert("那是自然");}
+function cq13(){
+alert("勾勾勾（旁若无人地继续打鸣）");}
+function cq14(){
+alert("勾勾勾（旁若无人地继续打鸣");}
+
+function cq21(){
+alert("不理你");}
+function cq22(){
+alert("不理你");}
+function cq23(){
+alert("还是不理你");}
+function cq24(){
+alert("(๑òᆺó๑)好吧，谢谢你，人类");}
+
+function dq11(){
+alert("✺◟(∗❛ัᴗ❛ั∗)◞✺期待");}
+function dq12(){
+alert("(๑>؂<๑）那一定很好吃");}
+function dq13(){
+alert("(๑• . •๑)好吧");}
+function dq14(){
+alert("（｡ò ∀ ó｡）嗯嗯！");}
+
+function dq21(){
+alert("好耶");}
+function dq22(){
+alert("Ծ‸Ծ好吧。。");}
+function dq23(){
+alert("(〃′o`)主人。。等我长大了，你可以卖我下的蛋");}
+function dq24(){
+alert("ヾ(＾。^*)哇主人你真好");}
